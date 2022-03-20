c4a65df50e6d8269a4a346559b08772a7c6abe1f
Nicholas Kaiser <nkaiser842@gmail.com>
2022-03-20 16:54:16 -0400

    changed quote system

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ var author = document.querySelector('.blockquote__text-credit')
 // var nav = document.querySelector('.nav')
 
 var loadQuotes = async function() {
-    var quote = await fetch(`https://api.quotable.io/random?maxLength=10/success`)
+    var quote = await fetch(`https://api.quotable.io/random?maxLength=75&&/tags=success`)
     var data = await quote.json()
     return data
  }
 
 var changeHTML = function(res) {
     quote.innerHTML = `${res.content}`
-    author.innherHTML = `${res.author}`
+    author.innerHTML = `${res.author}`
 }
 
 loadQuotes().then(res => changeHTML(res)).catch(err => console.error(err));
